b761dde8fcf8cbb6c423996cff7c891f178cecdb
Imran Abdullah <nagatoyahiko856@gmail.com>
2023-08-29 10:55:48 +0300

    Initial Commit

---
+++ chatting-app/chatting_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatting_appApp.swift
+//  chatting-app
+//
+//  Created by Imran Abdullah on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct chatting_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
